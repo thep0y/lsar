a35677007789b0bee2d3fc7dd592c69fcbb40cfb
thepoy <thepoy@163.com>
2022-02-18 20:17:38 +0800

    删除注释

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@ if (process.argv.length < 3) {
 } else {
     const roomID = process.argv.slice(2)[0]
     const c = new Crawler(parseInt(roomID))
-    // const c = new Crawler(4615502)
     c.printLiveLink().then().catch((e) => {
         console.log(e)
     })
