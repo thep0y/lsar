3926fc6438a10318be7b580a75a570d6fbbfcf06
thepoy <thepoy@163.com>
2022-08-31 22:48:00 +0800

    稍微格式化一下

--- src/apis/bilibili.ts
+++ src/apis/bilibili.ts
@@ -57,7 +57,16 @@ export class Bilibili extends Base {
   }
 
   private async parseRoomID() {
-    const res = await this.get(this.pageURL!, { 'Host': 'live.bilibili.com', 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:102.0) Gecko/20100101 Firefox/102.0', 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8', 'Accept-Language': 'zh-CN', 'Connection': 'keep-alive', 'Upgrade-Insecure-Requests': '1', 'Sec-Fetch-Dest': 'document', 'Sec-Fetch-Mode': 'navigate', 'Sec-Fetch-Site': 'none', 'Sec-Fetch-User': '?1', 'DNT': '1', 'Sec-GPC': '1' })
+    const res = await this.get(this.pageURL!, { 
+      'Host': 'live.bilibili.com', 
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:102.0) Gecko/20100101 Firefox/102.0', 
+      'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8', 
+      'Accept-Language': 'zh-CN', 'Connection': 'keep-alive', 
+      'Upgrade-Insecure-Requests': '1', 
+      'Sec-Fetch-Dest': 'document', 
+      'Sec-Fetch-Mode': 'navigate', 
+      'Sec-Fetch-Site': 'none', 'Sec-Fetch-User': '?1', 'DNT': '1', 'Sec-GPC': '1' 
+    })
     const findResult = res.match(/"defaultRoomId":"(\d+)"/)!
     return Number(findResult[1])
   }
