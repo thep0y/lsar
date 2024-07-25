d5bed0fb0046428c8b6a1f277a849f6038df6395
Ryan Wang <thepoy@163.com>
2024-07-25 22:19:34 +0800

    fix https://github.com/thep0y/lsar/issues/10

--- src/apis/douyu/index.ts
+++ src/apis/douyu/index.ts
@@ -1,25 +1,25 @@
-import { createHash } from 'crypto'
-import { CDNS, DOUYU_PROXY } from './consts'
-import { trace, debug, info, warn, error, fatal } from '../../logger'
-import { Base, printLink } from '..'
+import { createHash } from "node:crypto";
+import { CDNS, DOUYU_PROXY } from "./consts";
+import { trace, debug, info, warn, error, fatal } from "../../logger";
+import { Base, printLink } from "..";
 
-const did = '10000000000000000000000000001501'
+const did = "10000000000000000000000000001501";
 
 interface Info {
-  error?: number
-  msg: string
+  error?: number;
+  msg: string;
   data: {
-    rtmp_url: string
-    rtmp_live: string
-  }
+    rtmp_url: string;
+    rtmp_live?: string;
+  };
 }
 
 interface MobileResponse {
-  error: number
-  msg: string
+  error: number;
+  msg: string;
   data: {
-    url: string
-  }
+    url: string;
+  };
 }
 
 const infoString = (info: Info): string => {
@@ -27,126 +27,132 @@ const infoString = (info: Info): string => {
     error,
     msg,
     data: { rtmp_url, rtmp_live },
-  } = info
+  } = info;
   return JSON.stringify({
     error,
     msg,
     rtmp_url,
     rtmp_live,
-  })
-}
+  });
+};
 
-const NOT_LIVING_STATE = '房间未开播'
-const INVALID_REQUEST = '非法请求'
+const NOT_LIVING_STATE = "房间未开播";
+const INVALID_REQUEST = "非法请求";
 
 export class Douyu extends Base {
-  private isPost = true
+  private isPost = true;
   private ub98484234Reg = new RegExp(
-    /var vdwdae325w_64we.*?function ub98484234\(.*?return eval\(strc\)\(.*?\);\}/
-  )
-  private finalRoomID = 0
-  private signFunc = ''
+    /var vdwdae325w_64we.*?function ub98484234\(.*?return eval\(strc\)\(.*?\);\}/,
+  );
+  private finalRoomID = 0;
+  private signFunc = "";
 
-  baseURL = 'https://www.douyu.com/'
+  baseURL = "https://www.douyu.com/";
 
   get roomURL(): string {
-    return this.baseURL + this.roomID.toString()
+    return this.baseURL + this.roomID.toString();
   }
 
   private async getRoomInfo(params: string): Promise<Info | null> {
-    let url = ''
-    let resp = ''
+    let url = "";
+    let resp = "";
     if (this.isPost) {
-      url = `https://www.douyu.com/lapi/live/getH5Play/${this.finalRoomID}`
-      resp = await this.post(url, params)
+      url = `https://www.douyu.com/lapi/live/getH5Play/${this.finalRoomID}`;
+      resp = await this.post(url, params);
       if (!resp) {
-        warn('POST 请求未功能获得响应，更换 GET 请求重试')
-        return null
+        warn("POST 请求未功能获得响应，更换 GET 请求重试");
+        return null;
       }
     } else {
-      url = `https://playweb.douyu.com/lapi/live/getH5Play/${this.finalRoomID}?${params}`
-      resp = await this.get(url)
+      url = `https://playweb.douyu.com/lapi/live/getH5Play/${this.finalRoomID}?${params}`;
+      resp = await this.get(url);
       if (!resp) {
-        warn('GET 请求未功能获得响应，更换 POST 请求重试')
-        return null
+        warn("GET 请求未功能获得响应，更换 POST 请求重试");
+        return null;
       }
     }
 
-    const info = JSON.parse(resp) as Info
+    const info = JSON.parse(resp) as Info;
 
-    debug('有效响应体：', infoString(info))
+    debug("有效响应体：", infoString(info));
 
-    if (Object.hasOwn(info, 'error')) {
+    if (Object.hasOwn(info, "error")) {
       if (info.msg === NOT_LIVING_STATE) {
-        return fatal(`${this.roomID} ${NOT_LIVING_STATE}`)
-      } else if (info.msg === INVALID_REQUEST) {
-        error(INVALID_REQUEST)
+        return fatal(`${this.roomID} ${NOT_LIVING_STATE}`);
+      }
+
+      if (info.msg === INVALID_REQUEST) {
+        error(INVALID_REQUEST);
       }
     }
 
-    return info
+    return info;
   }
 
   private createParams(ts: number): string {
-    const signFunc = this.signFunc + `(${this.finalRoomID},"${did}",${ts})`
-    return eval(signFunc) as string
+    const signFunc = `${this.signFunc}(${this.finalRoomID},"${did}",${ts})`;
+    return eval(signFunc) as string;
   }
 
   private matchSignFunc(html: string) {
-    const matchResult = html.match(this.ub98484234Reg)
-    if (matchResult == null) throw Error('没找到函数 ub98484234')
+    const matchResult = html.match(this.ub98484234Reg);
+    if (matchResult == null) throw Error("没找到函数 ub98484234");
 
-    let ub98484234 = matchResult[0]
-    ub98484234 = ub98484234.replace(/eval\(strc\)\(\w+,\w+,.\w+\);/, 'strc;')
-    const ts = Math.floor(new Date().getTime() / 1e3)
-    const ub98484234Call = `ub98484234(${this.finalRoomID}, ${did}, ${ts})`
-    let signFunc = ''
+    let ub98484234 = matchResult[0];
+    ub98484234 = ub98484234.replace(/eval\(strc\)\(\w+,\w+,.\w+\);/, "strc;");
+    const ts = Math.floor(new Date().getTime() / 1e3);
+    const ub98484234Call = `ub98484234(${this.finalRoomID}, ${did}, ${ts})`;
+    let signFunc = "";
     try {
-      signFunc = eval(ub98484234 + ub98484234Call) as string
+      signFunc = eval(ub98484234 + ub98484234Call) as string;
     } catch (e) {
-      const slices = (e as Error).message.split(' ')
-      if (slices[slices.length - 1] === 'defined') {
-        const lossStr = `/var ${slices[0]}=.*?];/`
-        const lossReg = new RegExp(eval(lossStr) as string)
-        const matchResult = html.match(lossReg)
-        if (matchResult == null) throw Error('没找到函数 ub98484234')
+      const slices = (e as Error).message.split(" ");
+      if (slices[slices.length - 1] === "defined") {
+        const lossStr = `/var ${slices[0]}=.*?];/`;
+        const lossReg = new RegExp(eval(lossStr) as string);
+        const matchResult = html.match(lossReg);
+        if (matchResult == null) throw Error("没找到函数 ub98484234");
 
-        signFunc = eval(ub98484234 + matchResult[0] + ub98484234Call) as string
+        signFunc = eval(ub98484234 + matchResult[0] + ub98484234Call) as string;
       }
     }
-    const v = signFunc.match(/\w{12}/)!
-    const hash = createHash('md5')
-    hash.update(`${this.finalRoomID}${did}${ts}${v[0]}`)
-    const md5 = hash.digest('hex')
-    signFunc = signFunc.replace(/CryptoJS\.MD5\(cb\)\.toString\(\)/, `"${md5}"`)
-    signFunc = signFunc.split('return rt;})')[0] + 'return rt;})'
-    trace(signFunc)
-    return signFunc
+    const v = signFunc.match(/\w{12}/)!;
+    const hash = createHash("md5");
+    hash.update(`${this.finalRoomID}${did}${ts}${v[0]}`);
+    const md5 = hash.digest("hex");
+    signFunc = signFunc.replace(
+      /CryptoJS\.MD5\(cb\)\.toString\(\)/,
+      `"${md5}"`,
+    );
+    signFunc = `${signFunc.split("return rt;})")[0]}return rt;})`;
+    trace(signFunc);
+    return signFunc;
   }
 
   private async series(params: string) {
-    const info = await this.getRoomInfo(params)
-    return info
+    const info = await this.getRoomInfo(params);
+    return info;
   }
 
   private async params(): Promise<string> {
     if (!this.signFunc) {
-      const html = await this.getRoomPage()
+      const html = await this.getRoomPage();
 
-      const r = html.match(/\?room_id=(.+?)"/)
+      const r = html.match(/\$ROOM.room_id = (\d+);/);
       if (r) {
-        this.finalRoomID = Number(r[1].replaceAll(',', ''))
+        debug("正则查找房间 id 结果：", r[1]);
+        this.finalRoomID = Number(r[1]);
 
-        info('在网页中解析到最终房间 id：', this.finalRoomID)
+        info("在网页中解析到最终房间 id：", this.finalRoomID);
       }
 
-      this.signFunc = this.matchSignFunc(html)
+      this.signFunc = this.matchSignFunc(html);
     }
-    const ts = Math.floor(new Date().getTime() / 1e3)
-    const params = this.createParams(ts)
-    debug('请求参数：', params)
+    const ts = Math.floor(new Date().getTime() / 1e3);
+    const params = this.createParams(ts);
+    debug("请求参数：", params);
 
-    return params
+    return params;
   }
 
   private async getLiveName(params: string) {
@@ -159,99 +165,104 @@ export class Douyu extends Base {
             添加码率后的文件名为 {name}_{bit}.flv 或 {name}_{bit}.xs，
             不添加码率就会播放最高码率
         */
-    let info = await this.series(params)
-    let link_name = ''
+    let info = await this.series(params);
+    let link_name = "";
     if (info && info.error !== -15) {
-      if (info.data.rtmp_live == undefined) {
-        fatal(`${this.finalRoomID} 房间未开播`)
+      if (info.data.rtmp_live === undefined) {
+        fatal(`${this.finalRoomID} 房间未开播`);
+      } else {
+        link_name = info.data.rtmp_live
+          .split("?")[0]
+          .split(".")[0]
+          .split("_")[0];
       }
-      link_name = info.data.rtmp_live.split('?')[0].split('.')[0].split('_')[0]
     } else {
       /*
                斗鱼每个房间获取房间信息的请求方式随机变换，GET 和 POST 都有可能，
                所以这里请求失败时修改，但也只修改一次请求方式，如果仍失败就需要重新执行
             */
-      this.isPost = !this.isPost
-      info = await this.series(params)
+      this.isPost = !this.isPost;
+      info = await this.series(params);
       if (!info) {
         fatal(
-          '更换请求方式、生成新请求参数后仍未得到正确响应，请重新运行几次程序'
-        )
+          "更换请求方式、生成新请求参数后仍未得到正确响应，请重新运行几次程序",
+        );
       } else {
-        if (info.data.rtmp_live == undefined) {
-          fatal(`${this.finalRoomID} 房间未开播`)
+        if (info.data.rtmp_live === undefined) {
+          fatal(`${this.finalRoomID} 房间未开播`);
+        } else {
+          link_name = info.data.rtmp_live
+            .split("?")[0]
+            .split(".")[0]
+            .split("_")[0];
         }
-        link_name = info.data.rtmp_live
-          .split('?')[0]
-          .split('.')[0]
-          .split('_')[0]
       }
     }
 
     printLink({
-      tooltip: 'PC播放流',
+      tooltip: "PC播放流",
       link: `${info!.data.rtmp_url}/${info!.data.rtmp_live}`,
-      suffix: '\n',
-    })
+      suffix: "\n",
+    });
 
-    return link_name
+    return link_name;
   }
 
   private async getRoomPage() {
-    debug('当前房间链接：', this.roomURL)
-    return await this.get(this.roomURL)
+    debug("当前房间链接：", this.roomURL);
+    return await this.get(this.roomURL);
   }
 
   private async getMobileStream(params: string): Promise<string | undefined> {
-    const url = 'https://m.douyu.com/hgapi/livenc/room/getStreamUrl'
+    const url = "https://m.douyu.com/hgapi/livenc/room/getStreamUrl";
     const resp = await this.post(
       url,
-      params + `&rid=${String(this.finalRoomID)}&rate=-1`
-    )
+      `${params}&rid=${String(this.finalRoomID)}&rate=-1`,
+    );
 
-    const mr = JSON.parse(resp) as MobileResponse
+    const mr = JSON.parse(resp) as MobileResponse;
 
     if (mr.error !== 0) {
-      error('获取手机播放流出错：', mr.msg)
-      return
+      error("获取手机播放流出错：", mr.msg);
+      return;
     }
 
-    return mr.data.url
+    return mr.data.url;
   }
 
   async printLiveLink(): Promise<void> {
-    const params = await this.params()
+    const params = await this.params();
 
-    const mobileStream = await this.getMobileStream(params)
+    const mobileStream = await this.getMobileStream(params);
     if (mobileStream) {
-      console.log('优选：\n')
-      printLink({ tooltip: '手机播放流', link: mobileStream, suffix: '' })
+      console.log("优选：\n");
+      printLink({ tooltip: "手机播放流", link: mobileStream, suffix: "" });
     }
 
-    const name = await this.getLiveName(params)
+    const name = await this.getLiveName(params);
 
-    console.log('\n次选：选择下面的任意一条链接，播放失败换其他链接试试\n')
+    console.log("\n次选：选择下面的任意一条链接，播放失败换其他链接试试\n");
 
     for (const [cdn, config] of Object.entries(CDNS)) {
       for (const [prefix, format] of Object.entries(config)) {
-        const link = `http://${prefix}.${cdn}${name}.`
+        const link = `http://${prefix}.${cdn}${name}.`;
 
         if (format.flv) {
-          const flv_link = `${link}flv`
-          printLink({ link: flv_link })
+          const flv_link = `${link}flv`;
+          printLink({ link: flv_link });
         }
 
         if (format.m3u8) {
-          const m3u8_link = `${link}m3u8`
-          printLink({ link: m3u8_link })
+          const m3u8_link = `${link}m3u8`;
+          printLink({ link: m3u8_link });
         }
 
-        console.log('\n')
+        console.log("\n");
       }
     }
 
-    console.log('\n末选：上面 cdn 均不可用时，用下面的代理试试：\n')
-    const proxy = DOUYU_PROXY + this.roomID.toString()
-    printLink({ link: proxy })
+    console.log("\n末选：上面 cdn 均不可用时，用下面的代理试试：\n");
+    const proxy = DOUYU_PROXY + this.roomID.toString();
+    printLink({ link: proxy });
   }
 }
--- src/logger/logger.ts
+++ src/logger/logger.ts
@@ -3,85 +3,85 @@
  */
 export class Color {
   private base(code: number, msg: string): string {
-    return `\u001B[${code}m${msg}\u001B[39m`
+    return `\u001B[${code}m${msg}\u001B[39m`;
   }
   cyan(msg: string): string {
-    return this.base(36, msg)
+    return this.base(36, msg);
   }
 
   green(msg: string): string {
-    return this.base(32, msg)
+    return this.base(32, msg);
   }
 
   gray(msg: string): string {
-    return this.base(90, msg)
+    return this.base(90, msg);
   }
 
   red(msg: string): string {
-    return this.base(31, msg)
+    return this.base(31, msg);
   }
 
   yellow(msg: string): string {
-    return this.base(33, msg)
+    return this.base(33, msg);
   }
 
   purple(msg: string): string {
-    return this.base(35, msg)
+    return this.base(35, msg);
   }
 }
 
 export enum LoggerLevel {
   TRACE = 1,
-  DEBUG,
-  INFO,
-  WARNING,
-  ERROR,
-  FATAL,
+  DEBUG = 2,
+  INFO = 3,
+  WARNING = 4,
+  ERROR = 5,
+  FATAL = 6,
 }
 
-export type MsgArg = string | number | Error | object
+export type MsgArg = string | number | Error | object;
 
 const getLevelFromEnv = (): LoggerLevel => {
-  const level = process.env.LOG_LEVEL
+  const level = process.env.LOG_LEVEL;
 
   if (!level) {
-    return LoggerLevel.WARNING
+    return LoggerLevel.WARNING;
   }
 
   switch (level) {
-    case '1':
-      return LoggerLevel.TRACE
-    case '2':
-      return LoggerLevel.DEBUG
-    case '3':
-      return LoggerLevel.INFO
-    case '5':
-      return LoggerLevel.ERROR
-    case '6':
-      return LoggerLevel.TRACE
+    case "1":
+      return LoggerLevel.TRACE;
+    case "2":
+      return LoggerLevel.DEBUG;
+    case "3":
+      return LoggerLevel.INFO;
+    case "5":
+      return LoggerLevel.ERROR;
+    case "6":
+      return LoggerLevel.TRACE;
     default:
-      return LoggerLevel.WARNING
+      return LoggerLevel.WARNING;
   }
-}
+};
 
-export const defaultColor = new Color()
+export const defaultColor = new Color();
 
 interface Arg {
-  addDate?: boolean
-  color?: Color
-  level?: LoggerLevel
-  maxLength?: number
+  addDate?: boolean;
+  color?: Color;
+  level?: LoggerLevel;
+  maxLength?: number;
 }
 
 /**
  * 彩色日志
  */
 export class Logger {
-  private color: Color
-  private addDate = false
-  private level: LoggerLevel
-  private prefixes: Record<LoggerLevel, string>
-  private maxLength: number | undefined = undefined
+  private color: Color;
+  private addDate = false;
+  private level: LoggerLevel;
+  private prefixes: Record<LoggerLevel, string>;
+  private maxLength: number | undefined = undefined;
 
   constructor({
     addDate = false,
@@ -89,139 +89,139 @@ export class Logger {
     level,
     maxLength,
   }: Arg) {
-    this.addDate = addDate
-    this.color = color
-    this.level = level ?? getLevelFromEnv()
-    this.maxLength = maxLength
+    this.addDate = addDate;
+    this.color = color;
+    this.level = level ?? getLevelFromEnv();
+    this.maxLength = maxLength;
 
     this.prefixes = {
-      1: this.color.gray('TRC'),
-      2: this.color.purple('DEB'),
-      3: this.color.green('INF'),
-      4: this.color.yellow('WAR'),
-      5: this.color.red('ERR'),
-      6: this.color.red('FAT'),
-    }
+      1: this.color.gray("TRC"),
+      2: this.color.purple("DEB"),
+      3: this.color.green("INF"),
+      4: this.color.yellow("WAR"),
+      5: this.color.red("ERR"),
+      6: this.color.red("FAT"),
+    };
   }
 
   private _time(): string {
     if (this.addDate) {
-      return this.datetime()
+      return this.datetime();
     }
-    return this.time()
+    return this.time();
   }
 
   private datetime(): string {
-    const now = new Date(Date.now())
+    const now = new Date(Date.now());
 
-    let mon: string | number = now.getMonth()
-    let date: string | number = now.getDate()
-    let hour: string | number = now.getHours()
-    let min: string | number = now.getMinutes()
-    let sec: string | number = now.getSeconds()
-    mon = mon < 10 ? '0' + mon.toString() : mon
-    date = date < 10 ? '0' + date.toString() : date
-    hour = hour < 10 ? '0' + hour.toString() : hour
-    min = min < 10 ? '0' + min.toString() : min
-    sec = sec < 10 ? '0' + sec.toString() : sec
+    let mon: string | number = now.getMonth();
+    let date: string | number = now.getDate();
+    let hour: string | number = now.getHours();
+    let min: string | number = now.getMinutes();
+    let sec: string | number = now.getSeconds();
+    mon = mon < 10 ? `0${mon.toString()}` : mon;
+    date = date < 10 ? `0${date.toString()}` : date;
+    hour = hour < 10 ? `0${hour.toString()}` : hour;
+    min = min < 10 ? `0${min.toString()}` : min;
+    sec = sec < 10 ? `0${sec.toString()}` : sec;
 
     return this.color.gray(
-      `${now.getFullYear()}-${mon}-${date} ${hour}:${min}:${sec}`
-    )
+      `${now.getFullYear()}-${mon}-${date} ${hour}:${min}:${sec}`,
+    );
   }
 
   private time(): string {
-    const now = new Date(Date.now())
+    const now = new Date(Date.now());
 
-    let hour: string | number = now.getHours()
-    let min: string | number = now.getMinutes()
-    let sec: string | number = now.getSeconds()
-    let mils: string | number = now.getMilliseconds()
-    hour = hour < 10 ? '0' + hour.toString() : hour
-    min = min < 10 ? '0' + min.toString() : min
-    sec = sec < 10 ? '0' + sec.toString() : sec
+    let hour: string | number = now.getHours();
+    let min: string | number = now.getMinutes();
+    let sec: string | number = now.getSeconds();
+    let mils: string | number = now.getMilliseconds();
+    hour = hour < 10 ? "0" + hour.toString() : hour;
+    min = min < 10 ? "0" + min.toString() : min;
+    sec = sec < 10 ? "0" + sec.toString() : sec;
     mils =
       mils < 100
         ? mils < 10
-          ? '00' + mils.toString()
-          : '0' + mils.toString()
-        : mils
+          ? "00" + mils.toString()
+          : "0" + mils.toString()
+        : mils;
 
-    return this.color.gray(`${hour}:${min}:${sec}.${mils}`)
+    return this.color.gray(`${hour}:${min}:${sec}.${mils}`);
   }
 
   private mergeMsgs(msg: string, msgs: MsgArg[]): string {
     msgs.forEach((v, i) => {
       if (!this.maxLength) {
-        const arg = typeof v === 'string' ? v.trim() : v.toString().trim()
-        msg += ` ${arg}`
+        const arg = typeof v === "string" ? v.trim() : v.toString().trim();
+        msg += ` ${arg}`;
       } else {
-        const arg = typeof v === 'string' ? v.trim() : v.toString().trim()
+        const arg = typeof v === "string" ? v.trim() : v.toString().trim();
         const pure =
           i > 0
-            ? arg.replaceAll('\n', '').replaceAll(' ', '')
-            : arg.replaceAll('\n', '')
-        const len = pure.length
+            ? arg.replaceAll("\n", "").replaceAll(" ", "")
+            : arg.replaceAll("\n", "");
+        const len = pure.length;
 
         if (len > this.maxLength) {
           msg += ` ${pure.slice(
             0,
-            this.maxLength
-          )}...(too long, length: ${len})`
+            this.maxLength,
+          )}...(too long, length: ${len})`;
         } else {
-          msg += ' ' + pure
+          msg += " " + pure;
         }
       }
-    })
-    return msg
+    });
+    return msg;
   }
 
   private baseMsg(level: LoggerLevel): string {
-    return `${this._time()} ${this.prefixes[level]} ${this.color.cyan('>')}`
+    return `${this._time()} ${this.prefixes[level]} ${this.color.cyan(">")}`;
   }
 
   trace(...msgs: MsgArg[]) {
     if (LoggerLevel.TRACE >= this.level) {
-      console.log(this.mergeMsgs(this.baseMsg(LoggerLevel.TRACE), msgs))
+      console.log(this.mergeMsgs(this.baseMsg(LoggerLevel.TRACE), msgs));
     }
   }
 
   debug(...msgs: MsgArg[]) {
     if (LoggerLevel.DEBUG >= this.level) {
-      console.log(this.mergeMsgs(this.baseMsg(LoggerLevel.DEBUG), msgs))
+      console.log(this.mergeMsgs(this.baseMsg(LoggerLevel.DEBUG), msgs));
     }
   }
 
   info(...msgs: MsgArg[]) {
     if (LoggerLevel.INFO >= this.level) {
-      console.log(this.mergeMsgs(this.baseMsg(LoggerLevel.INFO), msgs))
+      console.log(this.mergeMsgs(this.baseMsg(LoggerLevel.INFO), msgs));
     }
   }
 
   error(...msgs: MsgArg[]) {
     if (LoggerLevel.ERROR >= this.level) {
-      console.log(this.mergeMsgs(this.baseMsg(LoggerLevel.ERROR), msgs))
+      console.log(this.mergeMsgs(this.baseMsg(LoggerLevel.ERROR), msgs));
     }
   }
 
   warn(...msgs: MsgArg[]) {
     if (LoggerLevel.WARNING >= this.level) {
-      console.log(this.mergeMsgs(this.baseMsg(LoggerLevel.WARNING), msgs))
+      console.log(this.mergeMsgs(this.baseMsg(LoggerLevel.WARNING), msgs));
     }
   }
 
   fatal(...msgs: MsgArg[]): never {
-    console.log(this.mergeMsgs(this.baseMsg(LoggerLevel.FATAL), msgs))
-    return process.exit(1)
+    console.log(this.mergeMsgs(this.baseMsg(LoggerLevel.FATAL), msgs));
+    return process.exit(1);
   }
 }
 
-export const log = new Logger({ maxLength: 30 })
+export const log = new Logger({});
 
-export const trace = (...msgs: MsgArg[]) => log.trace(...msgs)
-export const debug = (...msgs: MsgArg[]) => log.debug(...msgs)
-export const info = (...msgs: MsgArg[]) => log.info(...msgs)
-export const warn = (...msgs: MsgArg[]) => log.warn(...msgs)
-export const warning = warn
-export const error = (...msgs: MsgArg[]) => log.error(...msgs)
-export const fatal = (...msgs: MsgArg[]) => log.fatal(...msgs)
+export const trace = (...msgs: MsgArg[]) => log.trace(...msgs);
+export const debug = (...msgs: MsgArg[]) => log.debug(...msgs);
+export const info = (...msgs: MsgArg[]) => log.info(...msgs);
+export const warn = (...msgs: MsgArg[]) => log.warn(...msgs);
+export const warning = warn;
+export const error = (...msgs: MsgArg[]) => log.error(...msgs);
+export const fatal = (...msgs: MsgArg[]) => log.fatal(...msgs);
